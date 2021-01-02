fec35e0cc828dda11d6e58944bcbb2663fe1910f
limon100 <mahmudurlimon41@gmail.com>
2021-01-03 00:03:21 +0600

    Basketball score counter

--- Simple Score Counter/app/src/main/java/example/android/scorecounter/MainActivity.java
+++ Simple Score Counter/app/src/main/java/example/android/scorecounter/MainActivity.java
@@ -11,11 +11,13 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        //displayForTeamA(8);
     }
 
     public void addPoint3(View view){
@@ -24,15 +26,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addPoint2(View view){
-        displayForTeamA(2);
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addPointFree(View view){
-        displayForTeamA(1);
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addPoint3TeamB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addPoint2TeamB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addPointFreeTeamB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetButton(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }

452b31744c5d04e7b8b42f94b24c9c6fc5da2349
limon100 <mahmudurlimon41@gmail.com>
2021-01-07 10:46:01 +0600

    make all layout

--- Miwok/app/src/main/java/example/android/miwok/FamilyActivity.java
+++ Miwok/app/src/main/java/example/android/miwok/FamilyActivity.java
@@ -3,12 +3,37 @@ package example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.rootview);
+
+        WordAdapter adapter = new WordAdapter(this,  words);
+
+        ListView listView = (ListView) findViewById(R.id.list1);
+
+        listView.setAdapter(adapter);
+
     }
 }

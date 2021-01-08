23fe9361064658f259cf17b12d46e6cc798a00b7
limon100 <mahmudurlimon41@gmail.com>
2021-01-08 12:43:15 +0600

    set-up audio

--- Miwok/app/src/main/java/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/example/android/miwok/Word.java
@@ -5,20 +5,23 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId;
 
     private static final int NO_IMAGE_PROVIDED = -1;
     
-    public Word(String defaultTranslation , String miwokTrainslation){
+    public Word(String defaultTranslation , String miwokTrainslation , int audioResourceId){
 
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTrainslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation , String miwokTrainslation , int imageResourceId){
+    public Word(String defaultTranslation , String miwokTrainslation , int imageResourceId , int audioResourceId){
 
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTrainslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     public String getDefaultTranslation(){
@@ -35,6 +38,10 @@ public class Word {
 
     public boolean hasImage(){
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId(){
+        return mAudioResourceId;
     }
 
 }

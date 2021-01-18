2131c9b2e3386c0a8a170b28063716924e07aff7
limon100 <mahmudurlimon41@gmail.com>
2021-01-18 13:00:50 +0600

    adding spinner and menu option

--- Pet App/app/src/main/java/example/android/petsapp/EditorActivity.java
+++ Pet App/app/src/main/java/example/android/petsapp/EditorActivity.java
@@ -3,12 +3,28 @@ package example.android.petsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private EditText mNameEditText;
+    private EditText mBreedEditText;
+    private EditText mWeightEditText;
+    private Spinner mGenderSpinner;
+    private int mGender = PetEntry.GENDER_UNKNOWN;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mNameEditText = (EditText) findViewById(R.id.edit_pet_name);
+        mBreedEditText = (EditText) findViewById(R.id.edit_pet_breed);
+        mWeightEditText = (EditText) findViewById(R.id.edit_pet_weight);
+        mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+
+        setupSpinner();
     }
 }
--- Pet App/app/src/main/java/example/android/petsapp/MainActivity.java
+++ Pet App/app/src/main/java/example/android/petsapp/MainActivity.java
@@ -2,13 +2,30 @@ package example.android.petsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ///private PetDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fb = (FloatingActionButton)findViewById(R.id.fab);
+
+        fb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        //mDbHelper = new PetDbHelper(this);
     }
 }

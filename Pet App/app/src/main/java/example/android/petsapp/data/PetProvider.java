2ea50a9f3d23c3c3cfa48f05061b71dde2d60f4a
limon100 <mahmudurlimon41@gmail.com>
2021-01-20 12:58:14 +0600

    adding provider class:

---
+++ Pet App/app/src/main/java/example/android/petsapp/data/PetProvider.java
@@ -0,0 +1,47 @@
+package example.android.petsapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PetProvider extends ContentProvider {
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private PetDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri,  ContentValues values,  String selection,  String[] selectionArgs) {
+        return 0;
+    }
+}

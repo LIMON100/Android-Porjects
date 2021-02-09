face554ab4d6071f4ad019d92d655c92f68f47df
limon100 <mahmudurlimon41@gmail.com>
2021-02-09 12:57:51 +0600

    messege to everyone

---
+++ Squawker/app/src/main/java/android/example/com/squawker/provider/SquawkDatabase.java
@@ -0,0 +1,15 @@
+package android.example.com.squawker.provider;
+
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
+
+@Database(version = SquawkDatabase.VERSION)
+public class SquawkDatabase {
+
+    public static final int VERSION = 4;
+
+    @Table(SquawkContract.class)
+    public static final String SQUAWK_MESSAGES = "squawk_messages";
+
+}
